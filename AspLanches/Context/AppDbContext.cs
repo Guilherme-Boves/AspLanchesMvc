c191490b65d1ca4b6e690795541d0e33c21db228
Guilherme Boves <ghboves@gmail.com>
2023-06-21 15:41:48 -0300

    Criação dos models Categoria e Lanche. Configuração do Entity framework

---
+++ AspLanches/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AspLanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Categoria> Categorias { get; set; }
+        DbSet<Lanche> Lanches { get; set; }
+    }
+}
