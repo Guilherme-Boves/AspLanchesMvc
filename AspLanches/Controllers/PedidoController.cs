22194b5de546f839e4cfd44b1b53f95b840df7ea
Guilherme Boves <ghboves@gmail.com>
2023-06-26 14:29:29 -0300

    Criando o método Action Checkout [HttpPost]

--- AspLanches/Controllers/PedidoController.cs
+++ AspLanches/Controllers/PedidoController.cs
@@ -23,7 +23,50 @@ namespace AspLanches.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Obter os itens do carrinho de compra do cliente
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //Verifica se existem itens de pedido
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio");
+            }
+
+            //calcula o total de itens e o total do pedido
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //Atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //Valida os dados do pedido
+            if (ModelState.IsValid)
+            {
+                //Cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //Define mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+
+            return View(pedido);
+
         }
     }
 }
