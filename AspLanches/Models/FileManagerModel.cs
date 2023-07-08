688320326adbec95328be59ff30b49e1cff3e213
Guilherme Boves <ghboves@gmail.com>
2023-07-08 16:44:36 -0300

    Enviando, exibindo e excluindo imagens

--- AspLanches/Models/FileManagerModel.cs
+++ AspLanches/Models/FileManagerModel.cs
@@ -2,5 +2,9 @@
 {
     public class FileManagerModel
     {
+        public FileInfo[] Files { get; set; }
+        public IFormFile IFormFile { get; set; }
+        public List<IFormFile> IFormFiles { get; set; }
+        public string PathImagesProduto { get; set; }
     }
 }
